1b5769221d1b45761ef3a36cbbc8dee39349c7b3
vishalsiwach59 <>
2020-10-16 17:51:53 +0530

    Add prefix NEGSUPPORT

--- sqrt.c
+++ sqrt.c
@@ -15,13 +15,13 @@ int main(int argc, char* argv[]) {
 
 	int input = atoi(argv[1]);
 	if(input<0){
-		printf("Sqrt of %d is %fi\n",input,sqrt(-1*input));
+		printf("NEGSUPPORT: Sqrt of %d is %fi\n",input,sqrt(-1*input));
 	}
 	else{
-		printf("Sqrt of %d is %f\n",input,sqrt(input));
+		printf("NEGSUPPORT:Sqrt of %d is %f\n",input,sqrt(input));
 	}
 
-	printf("End of Program.Exiting\n");
+	printf("NEGSUPPORT:End of Program.Exiting\n");
 	return(0);
 
 } // end main
