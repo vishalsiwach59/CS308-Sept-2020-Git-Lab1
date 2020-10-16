bef93058d73fc8e8e421b320061f6af5a196924e
vishalsiwach59 <>
2020-10-16 17:49:09 +0530

    Add support for sqrt of -ve no.s

--- sqrt.c
+++ sqrt.c
@@ -15,10 +15,11 @@ int main(int argc, char* argv[]) {
 
 	int input = atoi(argv[1]);
 	if(input<0){
-		printf("Negative no. - invalid for sq root\n");
-		exit(-1);
+		printf("Sqrt of %d is %fi\n",input,sqrt(-1*input));
 	}
-	printf("Sqrt of %d is %f\n",input,sqrt(input));
+	else{
+		printf("Sqrt of %d is %f\n",input,sqrt(input));
+	}
 
 	printf("End of Program.Exiting\n");
 	return(0);
